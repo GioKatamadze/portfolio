f2279526b1286030352362841a9a2938847d8650
Gio Katamadze <giokat.inbox@gmail.com>
2023-01-17 20:10:09 +0400

    remove github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/redux.js";
@@ -9,8 +9,8 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
